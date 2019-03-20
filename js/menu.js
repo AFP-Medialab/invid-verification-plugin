7f1836a32d2eeab184b8908560b05f88ee5b86ff
laurent-lacroix <lacroix.laurent@gmail.com>
2019-03-20 15:36:55 +0100

    Preparation of next release

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,8 @@
 * Set here your own Tracking ID 
 */
 var trackingID = "UA-XXXXXXXX-Y";
-var page_name = 'invid.html';
-// var page_name = 'we-verify.html';
+// var page_name = 'invid.html';
+var page_name = 'we-verify.html';
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
