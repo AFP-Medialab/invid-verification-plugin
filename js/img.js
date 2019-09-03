b74f738b7ac227c38251d3c9a731d62773e8a7b2
Thibault <thibault.lhuillier@epita.fr>
2019-09-03 15:11:02 +0200

    #53

--- js/img.js
+++ js/img.js
@@ -104,7 +104,7 @@ function rebuild(img_url)
 */
 function ValidURL(str) 
 {
-    var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    var regex = /(http|https|blob:chrome-extension):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
     return regex.test(str);
 }
 
@@ -137,11 +137,16 @@ function submit_img()
         if( ! ValidURL(img_url) ) {
             document.getElementById("lst_search_btn").setAttribute("style", "display: none");
             document.getElementById("magnifier-content").style.display = (histo.local_path) ? "" : "none";
+            console.log(histo.local_path)
         }
         else {
             document.getElementById("lst_search_btn").setAttribute("style", "display: inline");
             document.getElementById("magnifier-content").style.display = "";
         }
+    }
+    else
+    {
+        document.getElementById("magnifier-content").style.display = "none";
     }
 }
 
@@ -282,7 +287,7 @@ if( form){
     form.addEventListener("submit", apply_filter, false);
     form.addEventListener("submit", function(e) {
         e.preventDefault();
-    });    
+    });
 }
 
 /**
