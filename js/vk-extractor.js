6b0243c93369d4855fec99aa06424c1f62d06c96
juliette <juliette.casteran@epita.fr>
2019-09-05 12:15:11 +0200

    #57: Can now scrap a page with several videos

--- js/vk-extractor.js
+++ js/vk-extractor.js
@@ -11,42 +11,78 @@
 	vkExtractor.validUrl = function (url) {
 		return url.startsWith("https://vk.com/");
 	}
+    
+    function makeHttpObject() {
+        try {return new XMLHttpRequest();}
+        catch (error) {}
+        try {return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (error) {}
+        try {return new ActiveXObject("Microsoft.XMLHTTP");}
+        catch (error) {}
+      
+        throw new Error("Could not create HTTP request object.");
+    }
 
+    function extractOne(videoUrl) {
+        var mp4Url = "";
+        return new Promise((resolve, reject) => {
+        var sourceHTML;
+        var request = makeHttpObject();
+        request.open("GET", videoUrl, true);
+        request.send(null);
+        request.onreadystatechange = function() {
+            if (request.readyState == 4){
+                sourceHTML = request.responseText;
+
+                mp4Url = sourceHTML.split(/url240\"/)[1];
+
+                mp4Url = mp4Url.split("\"")[1];
+                resolve(mp4Url);
+            }
+        };
+    }).then(() => { return mp4Url })
+    .catch((err) => window.alert(err));
+    }
 	vkExtractor.extract = function (url, callback) {
        
         if (url.match(/https:\/\/vk\.com\/.*\?z=video-/)){
             var videoUrl = "https://vk.com/" + getId(url);
-            function makeHttpObject() {
-                try {return new XMLHttpRequest();}
-                catch (error) {}
-                try {return new ActiveXObject("Msxml2.XMLHTTP");}
-                catch (error) {}
-                try {return new ActiveXObject("Microsoft.XMLHTTP");}
-                catch (error) {}
-              
-                throw new Error("Could not create HTTP request object.");
-            } 
-            var sourceHTML;
-            var mp4Url = "";
-            var request = makeHttpObject();
-            request.open("GET", videoUrl, true);
-            request.send(null);
-            request.onreadystatechange = function() {
-                if (request.readyState == 4){
-                    sourceHTML = request.responseText;
-
-                    var mp4Url = sourceHTML.split(/url240\"/)[1];
-
-                    mp4Url = mp4Url.split("\"")[1];
-                    
-                   callback(mp4Url);
-                }
-            };
-
+            extractOne(videoUrl).then(video => {window.alert(video); callback(video) });
+           
            // var page = get_page(videoUrl);
 
             //ar regexExtractor = /<source [^>]*src="([^"]+)"[^>]*>/;
             //callback(page.match(regexExtractor)[1]);
+
+        }
+        else
+        {
+            var promise = new Promise((resolve, reject) => {
+                var request = makeHttpObject();
+                request.open("GET", url, true);
+                request.send(null);
+                request.onreadystatechange = function() {
+                    if (request.readyState == 4){
+                        var videos = request.responseText
+                                                .match(/video-[0-9]*_[0-9]*/g)
+                                                .filter((value, index, self) => {
+                                                    return self.indexOf(value) === index;
+                                                });
+                        
+                        window.alert(videos);
+                        videos.forEach( video => { 
+                            extractOne("https://vk.com/" + video)
+                                .then(url => {
+                                        callback(url);
+                                }); 
+
+                        });
+
+                        resolve();
+
+                    }
+                }
+            });
 
         }
 	}
