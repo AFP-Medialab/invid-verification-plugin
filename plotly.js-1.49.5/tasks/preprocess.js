3bb516b16af96b4935c6b862220176cfcef8ee8f
juliette <juliette.casteran@epita.fr>
2019-09-25 11:53:23 +0200

    Add plotly files

---
+++ plotly.js-1.49.5/tasks/preprocess.js
@@ -0,0 +1,36 @@
+var fs = require('fs-extra');
+var sass = require('node-sass');
+
+var constants = require('./util/constants');
+var common = require('./util/common');
+var pullCSS = require('./util/pull_css');
+var updateVersion = require('./util/update_version');
+
+// main
+makeBuildCSS();
+copyTopojsonFiles();
+updateVersion(constants.pathToPlotlyCore);
+updateVersion(constants.pathToPlotlyGeoAssetsSrc);
+
+// convert scss to css to js
+function makeBuildCSS() {
+    sass.render({
+        file: constants.pathToSCSS,
+        outputStyle: 'compressed'
+    }, function(err, result) {
+        if(err) throw err;
+
+        // css to js
+        pullCSS(String(result.css), constants.pathToCSSBuild);
+    });
+}
+
+// copy topojson files from sane-topojson to dist/
+function copyTopojsonFiles() {
+    fs.copy(
+        constants.pathToTopojsonSrc,
+        constants.pathToTopojsonDist,
+        { clobber: true },
+        common.throwOnError
+    );
+}
