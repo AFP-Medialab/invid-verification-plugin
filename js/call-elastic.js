31c3c57cf83751cbf18cafd6914169c4e47d0b7b
juliette <juliette.casteran@epita.fr>
2019-10-22 13:35:45 +0200

    Graphs tweets display: fixed adding listener

--- js/call-elastic.js
+++ js/call-elastic.js
@@ -353,8 +353,6 @@ export function generateTweetCount(param) {
         });
         const myJson = await response.json();
 
-        console.log("MY JSON");
-        console.log(myJson);
         json = myJson;
         return myJson["hits"]["total"];
     };
@@ -367,7 +365,6 @@ export function generateURLArray(param) {
     let aggs = constructAggs("urls");
 
     let query = JSON.stringify(buildQuery(aggs, must)).replace(/\\/g, "").replace(/\"{/g, "{").replace(/}\"/g, "}");
-    console.log(query);
     const userAction = async () => {
         const response = await fetch(elasticSearch_url, {
             method: 'POST',
@@ -387,8 +384,6 @@ export function generateURLArray(param) {
             '<td>url</td>' +
             '<td>count</td>' +
             '</tr>';
-
-            console.log(array);
         array.forEach(row => {
             arrayStr += '<tr>' +
                 '<td><a href="' + row.url + '" target="_blank">' + row.url + '</a></td>' +
@@ -411,7 +406,6 @@ function mostTweetsGet(key, values, labels, parents, mainKey) {
 function getURLArray(json) {
     var urlArray = [];
     var buckets = json["aggregations"]["2"]["buckets"];
-    console.log(json);
     buckets.forEach(bucket => {
         urlArray.push({ url: bucket["key"], count: bucket["doc_count"] });
     });
